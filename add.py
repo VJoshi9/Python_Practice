3b36854d79ea609d93f980c455a6eab8779be582
vinayjoshi <joshivinay085@gmail.com>
2022-10-01 15:47:56 +0530

    adding two numbers

---
+++ add.py
@@ -0,0 +1,16 @@
+#Create two variables (x and y ) and assign values to them
+
+x = 5
+y = 7
+
+#Create a third variable (sum) and add x and y
+
+sum = x + y
+
+# print the sum
+
+print ("The answer is: ", sum)
+
+
+
+#
