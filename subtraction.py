b7e014646437c7e94854854be70a1db7974322aa
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-10-01 16:42:34 +0530

    Add files via upload

---
+++ subtraction.py
@@ -0,0 +1,4 @@
+firstNumber = input("Enter first number : ")
+secondNumber = input("Enter second number : ")
+subtraction = float(firstNumber)-float(secondNumber)
+print("The answer is: ", subtraction)
