adfd0a802bdb79779e629b444b879014cacaa7df
Vinay Kumar Joshi <joshivinay085@gmail.com>
2022-10-01 16:38:30 +0530

    Add files via upload

---
+++ swapFunction.py
@@ -0,0 +1,6 @@
+#Take inputs from user for two numbers (x and y)
+a = int(input("Enter first number: "))
+b = int(input("Enter another number: "))
+print("The numbers are a,b = ", a,b )
+a,b = b,a
+print("The numbers after swapping are a,b= ", a,b) 
